74f4b86c51630d373fe7cc1ebcc46349b1d4d32e
AngeloRoudani <ange.rouda@gmail.com>
2023-05-04 15:56:43 +0200

    modifiche milestone base

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ require __DIR__ . '/Models/movie.php';
 
 $starWars = new Movie('Star Wars', 'Fantascienza', 'LucasFilms', '1985', 'USA', '4.8', 'English' );
 
-echo $starWars->getMovieInfo();
 
 ?>
 
