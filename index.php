cc90f61eb6e5fbd3e712099268805ac0a4b56b6b
AngeloRoudani <ange.rouda@gmail.com>
2023-05-04 15:50:46 +0200

    milestone base

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 
 require __DIR__ . '/Models/movie.php';
+
+$starWars = new Movie('Star Wars', 'Fantascienza', 'LucasFilms', '1985', 'USA', '4.8', 'English' );
+
+echo $starWars->getMovieInfo();
+
+?>
--- models/movie.php
+++ models/movie.php
@@ -1,6 +1,29 @@
 <?php
 
 class Movie {
-    public $name;
-    public $
+    public $title;
+    public $genre;
+    public $productionHouse;
+    public $releaseDate;
+    public $productionCountry;
+    public $vote;
+    public $originalLanguage;
+
+
+    public function __construct($title, $genre, $productionHouse, $productionCountry, $releaseDate, $vote, $originalLanguage ) {
+        
+        $this->title = $title;
+        $this->genre = $genre;
+        $this->productionHouse = $productionHouse;
+        $this->productionCountry = $productionCountry;
+        $this->releaseDate = $releaseDate;
+        $this->vote = $vote;
+        $this->originalLanguage = $originalLanguage;
+
+    }
+
+    public function getMovieInfo() {
+        return '<h1>'. $this->title . '<h1>';
+    }
+
 }
